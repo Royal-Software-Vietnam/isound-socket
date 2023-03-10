b75a2bc83ffd87c0b374a6280eaa406db0766312
Hwang <rsoft.hwang99@gmail.com>
2023-03-10 16:44:13 +0700

    [new] allow to upload

--- dist/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-exports.default = new sequelize_1.Sequelize("isound", process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "mysql"
-});
